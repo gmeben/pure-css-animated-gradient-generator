4e37d2f5e4f136c5da7ddc96f7e9c25cec14b4be
Grant Eben <gmeben@gmail.com>
2021-07-03 13:25:52 -0400

    build: Upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,22 @@
 const path = require('path')
-const HTMLplugin = require('html-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const rules = [
-    {
-        test: /\.js$/,
-        exclude: /node_module/,
-        use: {
-            loader: 'babel-loader'
+        {
+            test: /\.js$/,
+            exclude: /node_module/,
+            use: {
+                loader: 'babel-loader'
+            }
+        },
+        {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+                'style-loader', 'css-loader'
+            ]
         }
-    },
-    {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-            'style-loader', 'css-loader'
-        ]
-    }
-]
+    ]
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.js'),
